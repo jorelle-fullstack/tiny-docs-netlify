cf7ca26249ac1e91f8b690cbb59198693384b338
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-23 17:42:35 +0800

    Added Registration to Firebase

--- pages/api/firebase/loginWithFacebook.js
+++ pages/api/firebase/loginWithFacebook.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import "../../../services/firebase"
+import "firebase/firestore";
 import Router from "next/router";
 
 const LoginWithFacebook = () => {
@@ -9,7 +10,32 @@ const LoginWithFacebook = () => {
             firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(
                 (userCred) => {
                     //Do something if login is successful
-                  console.log(userCred);
+                    console.log(userCred);
+                    var isNewUser = userCred.additionalUserInfo.isNewUser;
+                    // If user is new, save information to database
+                    try{
+                        if(isNewUser == true){
+                            firebase
+                            .firestore()
+                            .collection("users")
+                            .doc(userCred.user.uid)
+                            .set({
+                                firstName:  userCred.additionalUserInfo.profile.first_name,
+                                lastName: userCred.additionalUserInfo.profile.last_name,
+                                email: userCred.additionalUserInfo.profile.email,
+                                time_stamp: firebase.firestore.Timestamp.now()
+                            })
+                            .then(
+                                console.log("Data was successfully sent to cloud firestore")
+                            )
+                        }
+                        else{
+                            console.log("logged in successfully")
+                        }
+                    }
+                    catch(error){
+                        console.log(error);
+                    }
                 }
               )
         } catch (error){
