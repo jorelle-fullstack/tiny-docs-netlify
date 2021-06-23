71ef1f3ee1374da4d18d221268fee980167731e3
francis-fullstack <74534429+francis-fullstack@users.noreply.github.com>
2021-06-23 12:12:33 +0800

    setup firebase, simple login with facebook & google but data still not storing on database. data can check on console

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Link from 'next/link'
+import initFirebase from "../services/firebase";
 
 export default function Home() {
+  const loginWithGoogle = () => {
+    initFirebase.auth().signInWithPopup(new initFirebase.auth.GoogleAuthProvider()).then(
+      (userCred) => {
+        console.log(userCred);
+      }
+    )
+  }
+
+  const loginWithFacebook = () => {
+    initFirebase.auth().signInWithPopup(new initFirebase.auth.FacebookAuthProvider()).then(
+      (userCred) => {
+        console.log(userCred);
+      }
+    )
+  }
+
   return (
     <div className='page-home'>
      <Link href='other-page'> link to other page</Link>
+     <button onClick={loginWithGoogle}>Login With Google</button>
+    <button onClick={loginWithFacebook}>Login With Facebook</button>
     </div>
   )
 }
