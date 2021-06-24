d64702ec1d5e369599fbece2046590cb1ed47b2a
hazelle-fullstack <hazelle@fullstack.ph>
2021-06-24 16:22:04 +0800

    done: pricing page | pricing plans component

---
+++ pages/pricing/index.js
@@ -0,0 +1,13 @@
+import Plans from "../../components/global/Plans";
+
+const index = () => {
+  return (
+    <div className="page-pricing">
+      <div className="container">
+        <Plans />
+      </div>
+    </div>
+  )
+}
+
+export default index
