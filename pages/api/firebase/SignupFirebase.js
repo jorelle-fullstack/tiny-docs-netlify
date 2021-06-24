59cbf03e7b523f36f98214152049efa86244cc59
Khalvin Nucom <50348108+khalvin-fullstack@users.noreply.github.com>
2021-06-24 10:20:08 +0800

    Globals Fonts and Colors

--- pages/api/firebase/SignupFirebase.js
+++ pages/api/firebase/SignupFirebase.js
@@ -3,32 +3,32 @@ import '../../../services/firebase'
 import 'firebase/firestore'
 
 const SignupWithFirebase = (first_name, last_name, email, password) => {
-    const signUp = () => {
-        try{
-            firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
-                firebase
-                .firestore()
-                .collection("users")
-                .doc(user.user.uid)
-                .set({ //Set Collection Data
-                    firstName:  first_name,
-                    lastName: last_name,
-                    email: email,
-                    password: password,
-                    time_stamp: firebase.firestore.Timestamp.now()
-                })
-                .then(
-                    console.log(user)
-                )
-            })
-        }
-        catch(error){
-            console.log(error);
-        }
+  const signUp = () => {
+    try {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(user.user.uid)
+          .set({ //Set Collection Data
+            firstName: first_name,
+            lastName: last_name,
+            email: email,
+            password: password,
+            time_stamp: firebase.firestore.Timestamp.now()
+          })
+          .then(
+            console.log(user)
+          )
+      })
     }
-    return (
-        <button onClick={signUp}>Sign Up with Firebase</button>
-    )
+    catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <button onClick={signUp}>Sign Up with Firebase</button>
+  )
 }
 
 export default SignupWithFirebase
