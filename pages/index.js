ca006bc8c6cce6b2523146321fbe8df4098aa05a
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-24 15:45:31 +0800

    removed test button

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Link from 'next/link'
 import "../services/firebase"
 import firebase from 'firebase/app'
 import Router from "next/router";
-import LoginWithGoogle from './api/firebase/loginWithGoogle';
-import LoginWithFacebook from './api/firebase/loginWithFacebook';
 import SignupWithFirebase from './api/firebase/SignupFirebase';
 import LoginWithFirebase from './api/firebase/loginFirebase';
 
@@ -11,10 +9,7 @@ export default function Home() {
   return (
     <div className='page-home'>
      <Link href='other-page'> link to other page</Link>
-     <LoginWithGoogle/>
-     <LoginWithFacebook/>
-     <SignupWithFirebase/>
-     <LoginWithFirebase/>
+
     </div>
   )
 }
