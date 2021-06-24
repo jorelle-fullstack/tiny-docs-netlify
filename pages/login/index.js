cd5e387772be27a7c30e1101925751421d8fbae8
Khalvin Nucom <50348108+khalvin-fullstack@users.noreply.github.com>
2021-06-24 14:45:51 +0800

    refactor authentication

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,23 +1,42 @@
 import { Button } from '../../components/global'
 import { useForm } from "react-hook-form";
+import Link from 'next/link'
+import googleImg from '../../assets/images/google.svg'
+import fbImg from '../../assets/images/facebook.svg'
+import {login} from '../../auth'
 
 
 const index = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
 
+  console.log(googleImg)
+
   return (
     <div className='page-login'>
       <div className="first-wrapper">
-        <div className="form-holder">
-        <h1 className='title'>Login</h1>
+        <div className="content-holder">
+          <h1 className='title'>Login</h1>
           <form onSubmit={handleSubmit(onSubmit)} className='form'>
             <input defaultValue="test" {...register("email", { required: true })} type='email' />
             <input defaultValue="test" {...register("password", { required: true })} type='password' />
+            <Link href='/' passHref>
+              <a className='forgot'>Forgot Password</a>
+            </Link>
 
             {errors.email && <span>This field is required</span>}
             <Button className='btn--blue'>Login</Button>
           </form>
+
+          <p>- Or Login With - </p>
+
+          <div className="external-login-wrapper">
+            <Button onClick={e => login('google')}><img src={googleImg.src} alt="" /></Button>
+            <Button onClick={e => login('facebook')}><img src={fbImg.src} alt="" /></Button>
+          </div>
+          <Link href='/' passHref>
+            <a className='member'>Not a Member? Signu Up Now!</a>
+          </Link>
         </div>
       </div>
       <div className="second-wrapper">
