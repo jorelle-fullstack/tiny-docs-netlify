85b95dba4024d1137f4638d29a48630cca33939f
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-25 23:07:22 +0800

    created Stripe API

---
+++ pages/api/stripe/createPaymentIntent.js
@@ -0,0 +1,23 @@
+const Stripe = require('stripe');
+const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZwndQsbZFgLFekhF0YQF8pgf4UNQyIJPLNqgv54lhboVYImTJtbT00YvoXcMD1")
+
+export default async function handler (req, res) {
+
+    if (req.method === 'POST') {
+      const {paymentMethodType, currency} = req.body;
+      try{
+        const payment = await stripe.paymentIntents.create({
+          amount: 2000,
+          currency: currency,
+          payment_method_types: [paymentMethodType],
+          confirmation_method: 'manual',
+        });
+        res.json({ clientSecret: payment.client_secret});
+      }
+      catch(e){
+        res.status(400).json({ error: { message: e.message }})
+      }
+  } else {
+    
+  }
+}
