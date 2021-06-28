bb7fc3a805a414695b177c825f7a2b6e1a596633
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-28 16:30:16 +0800

    Added needed parameters

--- pages/api/stripe/createPaymentIntent.js
+++ pages/api/stripe/createPaymentIntent.js
@@ -4,21 +4,44 @@ const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZ
 export default async function handler (req, res) {
 
     if (req.method === 'POST') {
-      const {paymentMethodType, currency} = req.body;
-      try{
-        const payment = await stripe.paymentIntents.create({
-          amount: 2000,
-          currency: currency,
-          payment_method_types: [paymentMethodType],
-          confirmation_method: 'manual',
-          confirm:'true',
-          payment_method: 'pm_card_visa',
-        });
-        console.log(payment);
-      }
-      catch(e){
-        res.status(400).json({ error: { message: e.message }})
-      }
+      const {card_number,card_exp_month,card_exp_year,card_cvc,paymentMethodType,amount} = req.body;
+      //need card token before charge
+      //cardtoken -> charge-> payment intent
+
+      const token = await stripe.tokens.create({
+        card: {
+          number: card_number,
+          exp_month: card_exp_month,
+          exp_year: card_exp_year,
+          cvc: card_cvc,
+        },
+      });
+
+      const charge = await stripe.charges.create({
+        amount: amount, 
+        currency: "usd",
+        card: token,
+        description: "payinguser@example.com"
+      }, function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+            console.log(JSON.stringify(err, null, 2));
+        }
+      }).then(async function(){
+        try{
+          const payment = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "usd",
+            payment_method_types: [paymentMethodType],
+            confirmation_method: 'manual',
+            confirm:'true',
+            payment_method: 'pm_card_visa',
+          });
+          console.log(payment.status);
+        }
+        catch(e){
+          res.status(400).json({ error: { message: e.message }})
+        }
+      });
   } else {
     
   }
