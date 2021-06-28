68c419679d64fc05400f949b2bf9165c69bc87db
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-28 19:48:37 +0800

    Added Subscription Model

--- pages/api/stripe/createPaymentIntent.js
+++ pages/api/stripe/createPaymentIntent.js
@@ -4,11 +4,53 @@ const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZ
 export default async function handler (req, res) {
 
     if (req.method === 'POST') {
-      const {card_number,card_exp_month,card_exp_year,card_cvc,paymentMethodType,amount} = req.body;
-      //need card token before charge
-      //cardtoken -> charge-> payment intent
+      //// One Time Payment /////////////////////
 
-      const token = await stripe.tokens.create({
+      // const {card_number,card_exp_month,card_exp_year,card_cvc,paymentMethodType,amount} = req.body;
+      // //need card token before charge
+      // //cardtoken -> charge-> payment intent
+
+      // const token = await stripe.tokens.create({
+      //   card: {
+      //     number: card_number,
+      //     exp_month: card_exp_month,
+      //     exp_year: card_exp_year,
+      //     cvc: card_cvc,
+      //   },
+      // });
+
+      // const charge = await stripe.charges.create({
+      //   amount: amount, 
+      //   currency: "usd",
+      //   card: token,
+      //   description: "payinguser@example.com"
+      // }, function(err, charge) {
+      //   if (err && err.type === 'StripeCardError') {
+      //       console.log(JSON.stringify(err, null, 2));
+      //   }
+      // }).then(async function(){
+      //   try{
+      //     const payment = await stripe.paymentIntents.create({
+      //       amount: amount,
+      //       currency: "usd",
+      //       payment_method_types: [paymentMethodType],
+      //       confirmation_method: 'manual',
+      //       confirm:'true',
+      //       payment_method: 'pm_card_visa',
+      //     });
+         
+      //     res.status(200).json({ status: payment.status })
+      //   }
+      //   catch(e){
+      //     res.status(400).json({ error: { message: e.message }})
+      //   }
+      // });
+
+      ////Subscription Model /////////////////////
+      const {card_number,card_exp_month,card_exp_year,card_cvc,paymentMethodType,amount} = req.body;
+
+      const paymentMethod = await stripe.paymentMethods.create({
+        type: 'card',
         card: {
           number: card_number,
           exp_month: card_exp_month,
@@ -17,31 +59,44 @@ export default async function handler (req, res) {
         },
       });
 
-      const charge = await stripe.charges.create({
-        amount: amount, 
-        currency: "usd",
-        card: token,
-        description: "payinguser@example.com"
-      }, function(err, charge) {
-        if (err && err.type === 'StripeCardError') {
-            console.log(JSON.stringify(err, null, 2));
-        }
-      }).then(async function(){
-        try{
-          const payment = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: "usd",
-            payment_method_types: [paymentMethodType],
-            confirmation_method: 'manual',
-            confirm:'true',
-            payment_method: 'pm_card_visa',
+      await stripe.customers.create({
+        email: "sample@email.com",
+        name: "Jorelle Labanza",
+        phone: "0977711247",
+        description: 'Tiny Docs Subscriber',
+        payment_method: paymentMethod.id
+      }).then(async function(customer){
+
+        try {
+          const subscription = await stripe.subscriptions.create({
+            customer: customer.id,
+            items: [
+              {price: 'price_1J64PkAEed2wp5pxzfrVvtPP'},
+            ],
+            collection_method: "charge_automatically",
+            default_payment_method: paymentMethod.id
+          }).then(async function(){
+            try{
+              const payment = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: [paymentMethodType],
+                confirmation_method: 'automatic',
+                confirm:'true',
+                payment_method: 'pm_card_visa',
+              });
+            
+              res.status(200).json({ status: payment.status })
+            }
+            catch(e){
+              res.status(400).json({ error: { message: e.message }})
+            }
           });
-          console.log(payment.status);
-        }
-        catch(e){
+        }catch(e){
           res.status(400).json({ error: { message: e.message }})
         }
       });
+      
   } else {
     
   }
