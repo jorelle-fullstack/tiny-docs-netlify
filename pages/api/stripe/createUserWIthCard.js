eac2b58c24c3b3f4b9a1761581d1e3bdbab5fbe0
francis-fullstack <74534429+francis-fullstack@users.noreply.github.com>
2021-06-29 19:47:52 +0800

    comment firebase function

--- pages/api/stripe/createUserWIthCard.js
+++ pages/api/stripe/createUserWIthCard.js
@@ -1,9 +1,13 @@
+import firebase from 'firebase/app'
+import '../../../services/firebase'
+import 'firebase/firestore'
+
 const Stripe = require('stripe');
 const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZwndQsbZFgLFekhF0YQF8pgf4UNQyIJPLNqgv54lhboVYImTJtbT00YvoXcMD1")
 
 export default async function handler (req, res) {
   if (req.method === 'POST') {
-    const {card_number, card_exp_month, card_exp_year, card_cvc, first_name, last_name, email} = req.body;
+    const {card_number, card_exp_month, card_exp_year, card_cvc, first_name, last_name, email, uid} = req.body;
 
     const paymentMethod = await stripe.paymentMethods.create({
         type: 'card',
@@ -21,8 +25,12 @@ export default async function handler (req, res) {
         description: 'Tiny Docs Customer',
         payment_method: paymentMethod.id
       });
-
     res.status(200).json({customer_details: customer});
+    /* await firebase.firestore().collection('users').doc(userId).set({
+      subscription_id:
+    });
+ */
+
   }
   else {}
 }
