0443e7b744d2c1aa2c69561e62f177914687226a
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-30 13:44:42 +0800

    fixed coupon list

--- pages/api/stripe/subscription/getAllCoupon.js
+++ pages/api/stripe/subscription/getAllCoupon.js
@@ -1,9 +1,13 @@
 const Stripe = require('stripe');
 const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZwndQsbZFgLFekhF0YQF8pgf4UNQyIJPLNqgv54lhboVYImTJtbT00YvoXcMD1")
-
 export default async function handler (req, res) {
     const coupons = await stripe.coupons.list({
-      
       });
-    res.status(200).json({ coupon_list: coupons.id});
+    var all_coupons = [];
+    for(var i =0; i < Object.keys(coupons.data).length; i++){
+      
+      all_coupons[i] = coupons.data[i]['id'];
+
+    }
+    res.status(200).json({ coupon: all_coupons});
 }
