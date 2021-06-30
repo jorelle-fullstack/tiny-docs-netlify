953615eca65572cd298bbbdb86f41a47825329d6
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-30 11:26:54 +0800

    Add Discount Functionality

--- pages/api/stripe/addSubscription.js
+++ pages/api/stripe/addSubscription.js
@@ -3,8 +3,9 @@ const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZ
 
 export default async function handler (req, res) {
   if (req.method === 'POST') {
-    const {card_number, card_exp_month, card_exp_year, card_cvc, customer_id ,paymentMethodType, user_type} = req.body;
-
+    const {card_number, card_exp_month, card_exp_year, card_cvc, customer_id ,paymentMethodType, user_type, coupon} = req.body;
+    let price = "";
+    let amount = "";
     const paymentMethod = await stripe.paymentMethods.create({
       type: 'card',
       card: {
@@ -14,12 +15,19 @@ export default async function handler (req, res) {
         cvc: card_cvc,
       },
     });
+    await stripe.paymentMethods.attach(
+      paymentMethod.id,
+      {customer: customer_id}
+    );
+
     try {
       if (user_type == "family"){
         price = "price_1J64OtAEed2wp5pxeetSP29D";
+        amount = 1000;
       }
-      else if(amount == "pediatric"){
+      else if(user_type == "pediatric"){
         price = "price_1J64PkAEed2wp5pxzfrVvtPP";
+        amount = 2000;
       }
       const subscription = await stripe.subscriptions.create({
         customer: customer_id,
@@ -27,12 +35,15 @@ export default async function handler (req, res) {
           {price: price}, 
         ],
         collection_method: "charge_automatically",
-        default_payment_method: paymentMethod.id
+        default_payment_method: paymentMethod.id,
+        coupon: coupon
       })
       
-    }catch(e){
+    }
+    catch(e){
       res.status(400).json({ error: { message: e.message }})
     }
+
     try{
       const payment = await stripe.paymentIntents.create({
         amount: amount,
@@ -42,11 +53,12 @@ export default async function handler (req, res) {
         confirm:'true',
         payment_method: 'pm_card_visa',
       });
+
+      res.status(200).json({ payment_details: payment})
     }
     catch(e){
       res.status(400).json({ error: { message: e.message }})
     }
-    res.status(200).json({subscription_details: subscription});
   }
   else {}
 }
