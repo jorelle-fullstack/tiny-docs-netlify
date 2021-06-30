b213d97984d688aee0b71b54e3ec9e26fa3cd665
jorelle-fullstack <jorelle@fullstack.ph>
2021-06-30 11:57:09 +0800

    Add Coupon List

---
+++ pages/api/stripe/getAllCoupon.js
@@ -0,0 +1,9 @@
+const Stripe = require('stripe');
+const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZwndQsbZFgLFekhF0YQF8pgf4UNQyIJPLNqgv54lhboVYImTJtbT00YvoXcMD1")
+
+export default async function handler (req, res) {
+    const coupons = await stripe.coupons.list({
+      
+      });
+    res.status(200).json({ coupon_list: coupons});
+}
