840f5a6f7d4db41ff62c8ccde5fd3d46b504592c
jorelle-fullstack <jorelle@fullstack.ph>
2021-07-01 10:37:43 +0800

    env modification

--- pages/api/stripe/oneStepPayment.js
+++ pages/api/stripe/oneStepPayment.js
@@ -1,5 +1,5 @@
 const Stripe = require('stripe');
-const stripe = require("stripe")("sk_test_51J64FNAEed2wp5pxXJE1b3lDV7L36pPtjVUwZwndQsbZFgLFekhF0YQF8pgf4UNQyIJPLNqgv54lhboVYImTJtbT00YvoXcMD1")
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler (req, res) {
 
