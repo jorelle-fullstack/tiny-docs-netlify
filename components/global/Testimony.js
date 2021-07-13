c042edd66d372f9413df913cd8a6fd144b61944b
xenoxen <frolyn@fullstack.ph>
2021-07-13 17:25:18 +0800

    Fixed testimony component info text styling.

--- components/global/Testimony.js
+++ components/global/Testimony.js
@@ -5,13 +5,13 @@ import Quote from "../../assets/images/quote.svg"
 
 const Testimony = ({ index, name, desc, statement, infoColor }) => {
     return(
-        <div key={index} className={clsx({ "swiper-slide dark-blue": infoColor === 'dark blue', "swiper-slide green": infoColor === 'green' })}>
-        <div className="testimony__holder">
+        <div key={index} className={clsx('testimony', { "swiper-slide dark-blue": infoColor === 'dark blue', "swiper-slide green": infoColor === 'green' })}>
+        <div className="holder">
                     <Image src={Quote} alt="Quote" />
-                    <p className="testimony__statement">{statement}</p>
-                    <div className="testimony__info">
-                      <p className={clsx({ "testimony__name--dark": infoColor === 'dark blue', "testimony__name light": infoColor === 'green'})}>{name}</p>
-                      <p className="testimony__description--dark">{desc}</p>
+                    <p className="statement">{statement}</p>
+                    <div className="info">
+                      <p className={clsx('name', { "name--dark": infoColor === 'dark blue', "name--light": infoColor === 'green'})}>{name}</p>
+                      <p className={clsx('description', { "description--dark": infoColor === 'dark blue', "description--light": infoColor === 'green'})}>{desc}</p>
                     </div>
                   </div>
             </div>
