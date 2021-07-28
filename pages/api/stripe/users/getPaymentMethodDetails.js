41bb2cf6b7a3f077c97800f6ac7e232bb3b77249
jorelle-fullstack <jorelle@fullstack.ph>
2021-07-28 09:23:38 +0800

    Added get payment method endpoint

--- pages/api/stripe/users/getPaymentMethodDetails.js
+++ pages/api/stripe/users/getPaymentMethodDetails.js
@@ -2,8 +2,8 @@ const Stripe = require('stripe');
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 export default async function handler (req, res) {
     const { customer_id } = req.body;
-    const customer = await stripe.customers.retrieve(
-        customer_id
+    const paymentMethod = await stripe.paymentMethods.retrieve(
+      customer_id
     );
-    res.status(200).json({ customer_details: customer });
+    res.status(200).json({ payment_details: paymentMethod });
 }
